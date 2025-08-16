73170d9ece4ad56989bb28dcf6a2204f02c3523e
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-15 21:53:32 -0300

    Atualização

---
+++ Revisão/Loops/whileSair.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int opcao = 1;
+    int maior = 0;
+
+    while(opcao != 0){
+        printf("Digite um numero (0 para sair): ");
+        scanf("%d", &opcao);
+        if (opcao > maior){
+            maior = opcao;
+        }
+        printf("Número registrado!");
+    }
+    printf("O maior numero foi: %d", maior);
+}
