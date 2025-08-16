79ffc27dacfc9b6a397f9c39403a14b1c859075c
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-08-15 21:33:22 -0300

    Atualização

---
+++ Revisão/Loops/13e73.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int media, soma =0;
+    for(int i = 13 ; i < 74; i++){
+        soma += i;
+    }
+    media = soma / 60;
+    printf("A soam dos numeros foi de %d e sua media eh : %d" , soma,media);
+}
