ca19a554a0a9ad8945a5d7cba4398771fe8df0b7
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-05 17:33:43 -0300

    atualização

--- Ponteiros/Exercicios/produtoEscalar.c
+++ Ponteiros/Exercicios/produtoEscalar.c
@@ -2,23 +2,18 @@
 
 #define TAMANHO  5
 
-void soma(int *v, int n){
-    int contaI = 0;
-    int contaP = 0;
+void escalar(int *v1,int *v2 , int n){
+    int prod = 0;
     for(int i = 0; i < n;i++){
-        if(v[i] %2 == 0){
-            contaP++;
-        }else{
-            contaI++;
-        }
+        prod = v1[i] * v2[i];
     }
-    printf("Par -> %d\n", contaP);
-    printf("Impar -> %d\n", contaI);
+    printf("Produto escalar -> %d", prod);
 }
 
 int main()
 {
-    int vetor[TAMANHO] = {1,2,3,4,5};
-    soma(vetor, TAMANHO);
+    int vetor1[TAMANHO] = {1,2,3,4,5};
+    int vetor2[TAMANHO] = {1,2,3,4,5};
+    escalar(vetor1,vetor2, TAMANHO);
     return 0;
 }
