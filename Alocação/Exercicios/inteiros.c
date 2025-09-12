ad4e47ff3ec2360d44bd1c652520c747b845250b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-09-12 16:52:12 -0300

    atualização

---
+++ Alocação/Exercicios/inteiros.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *p;
+    p = (int *)malloc(5 * sizeof(int));
+    p[0] = 5;
+    p[1] = 5;
+    p[2] = 5;
+    p[3] = 5;
+    p[4] = 5;
+    p[5] = 5;
+    for(int i = 0; i < 10;i++){
+        printf("%d\n", p[i]);
+    }
+    return 0;
+}
