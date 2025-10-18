aacbf701036704fa8898745592b863c9df8eab3d
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2025-10-18 16:20:39 -0300

    Atualizar o fila_tamanho_fixo.c

--- C/Fila/fila_tamanho_fixo.c
+++ C/Fila/fila_tamanho_fixo.c
@@ -22,14 +22,28 @@ void adicionar(int valor){
 }
 
 void imprime(){
+    printf("Fila atualizada: \n");
     for(int i = 0 ; i <= fim; i++){
         printf("%d\n", FILA[i]);
+    }
+}
+
+void desenfilerar(){
+    if(inicio == 0){
+        printf("Fila vazia");
+    }else{
+        fim--;
+        FILA[0] = FILA[1];
+        FILA[1] = FILA[2];
     }
 }
 int main()
 {
     adicionar(10);
     adicionar(20);
+    adicionar(30);
+    imprime();
+    desenfilerar();
     imprime();
 
     return 0;
