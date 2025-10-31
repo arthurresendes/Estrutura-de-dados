8e0f6765835d75f0c954b13503a4358e726c6eee
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-31 19:38:55 -0300

    atualizar

---
+++ C/Fila/facul.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node{
+    int valor;
+    struct Node *prox;
+}Node;
+
+typedef struct  Fila
+{
+    Node *inicio;
+    Node * fim;
+    int tam;
+}Fila;
+
+
+void iniciailiza(Fila *f){
+    f->inicio = NULL;
+    f->fim = NULL;
+    f->tam = 0;
+}
+
+
+int vazia(Fila *f){
+    return  f->inicio == NULL;
+}
+
+void enqueue(int num,Fila *f){
+    Node *novo = malloc(sizeof(Node));
+
+    if(!novo){
+        exit(1);
+    }
+
+    novo->valor = num;
+    
+    if(vazia(f)){
+        f->inicio = novo;
+        f->fim = novo;
+    }else{
+        f->fim->prox = novo;
+        f->fim = novo;
+    }
+    f->tam--;
+    printf("Elemento %d enfileirado com sucesso!\n", num);
+}
+
+int dequeue(Fila *f){
+    if(vazia(f)){
+        printf("Fila vazia");
+        return -1;
+    }
+
+    Node *remover = f->inicio;
+    int valor = remover -> valor;
+    f->inicio = remover->prox;
+    if (f->inicio == NULL) {
+        f->fim = NULL;
+    }
+    free(remover);
+
+    f->tam--;
+
+    printf("Elemento %d removido do início da fila.\n", valor);
+    return valor;
+}
+
+
+void exibir(Fila *f){
+    if(vazia(f)){
+        printf("Fila vazia");
+        exit(2);
+    }
+
+
+    Node *aux = f->inicio;
+    int pos = 1;
+    while(aux != NULL){
+        printf("[%d] -> %d\n", pos, aux->valor);
+        aux = aux->prox;
+        pos++;
+    }
+    printf("------------------------------------\n\n");
+}
+
+int main(void) {
+    Fila f;
+    inicializa(&f);
+
+    int opcao, valor;
+
+    do {
+        printf("\n=========== FILA DINÂMICA EM C ===========\n");
+        printf("1 - Enfileirar (ENQUEUE)\n");
+        printf("2 - Desenfileirar (DEQUEUE)\n");
+        printf("3 - Exibir fila\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        printf("\n");
+
+        switch (opcao) {
+            case 1:
+                printf("Digite o valor a enfileirar: ");
+                scanf("%d", &valor);
+                enqueue(&f, valor);
+                break;
+            case 2:
+                dequeue(&f);
+                break;
+            case 3:
+                exibe(&f);
+                break;
+            case 0:
+                printf("Encerrando programa\n");
+                break;
+            default:
+                printf("Opcao invalida\n");
+        }
+
+    } while (opcao != 0);
+
+    libera(&f);
+    return 0;
+}
