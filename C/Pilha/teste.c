577600f4ecaa81144fc5f236f2c8bb86b50f1a25
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-10-31 18:40:27 -0300

    atualização

--- C/Pilha/teste.c
+++ C/Pilha/teste.c
@@ -1,81 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct pilha{
-    int dado;
-    struct pilha *prox;
-}Pilha;
+typedef struct Node
+{
+    int valor;
+    struct Node *prox;
+} Node;
 
-Pilha *topo = NULL;
+typedef struct Pilha
+{
+    Node *topo;
+    int tam;
+} Pilha;
 
-void pop();
-void push(int valor);
-void listagem();
+void inicializa(Pilha *p)
+{
+    p->topo = NULL;
+    p->tam = 0;
+};
 
-int main(){
-    int opcao = 0,n;
+void push(int valor, Pilha *p)
+{
+    Node *novo = malloc(sizeof(Node));
+    if (!novo)
+    {
+        printf("Sem memória disponível!\n");
+        exit(1);
+    }
+
+    novo->valor = valor;
+    novo->prox = p->topo;
+    p->topo = novo;
+    p->tam++;
+    printf("Elemento %d empilhado com sucesso!\n", valor);
+}
+
+int pop(Pilha *p)
+{
+    if (p->topo == NULL)
+    {
+        printf("Pilha vazia, nada para remover.\n");
+        return -1;
+    }
+    else
+    {
+        Node *remover = p->topo;
+        int valor = remover->valor;
+        p->topo = remover->prox;
+        free(remover);
+        p->tam--;
+        printf("Elemento %d removido do topo.\n", valor);
+        return valor;
+    }
+}
+
+
+void exibe(Pilha *p){
+    if (p->topo == NULL)
+    {
+        printf("Pilha vazia, nada para remover.\n");
+        return;
+    }else{
+        Node *aux = p->topo;
+        int pos = p->tam;
+        while(aux != NULL){
+            printf("[%d] -> %d\n", pos, aux->valor);
+            aux = aux->prox;
+        }
+    }
+}
+
+int main(void)
+{
+    Pilha p;
+    inicializa(&p); // Inicializa a pilha
+
+    int opcao, valor;
+
     do
     {
-        printf("\n=====Menu====\n");
-        printf("1-Listagem\n");
-        printf("2-Inserindo\n");
-        printf("3-Excluindo\n");
-        printf("0-Saindo\n");
-        printf(":");
+        printf("\n=========== PILHA DINÂMICA EM C ===========\n");
+        printf("1 - Empilhar (PUSH)\n");
+        printf("2 - Desempilhar (POP)\n");
+        printf("3 - Exibir pilha\n");
+        printf("4 - Zerar pilha\n");
+        printf("5 - Ver topo da pilha\n");
+        printf("6 - Quantos elementos na pilha\n");
+        printf("7 - Soma dos elementos da pilha\n");
+        printf("8 - Maior número da pilha\n");
+        printf("9 - Menor número da pilha\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
-        switch (opcao)
+        printf("\n");
+
+        if (opcao == 1)
         {
-        case 1:
-            listagem();
-            break;
-        case 2:
-            printf("Digite um valor numerio inteiro: ");
-            scanf("%d", &n);
-            push(n);
-            break;
-        case 3:
-            pop();
-            break;
-        case 0:
-            printf("Saindo... \n");
-            return 0;
-        default:
-            printf("Numero invalido\n");
-            break;
+            printf("Digite o valor a empilhar: ");
+            scanf("%d", &valor);
+            push(&p, valor);
         }
+        else if (opcao == 2)
+        {
+            pop(&p);
+        }
+        else if (opcao == 3)
+        {
+            exibe(&p);
+        }
+        else if (opcao == 4)
+        {
+            libera(&p);
+            printf("Pilha zerada!\n");
+        }
+        else if (opcao == 5)
+        {
+            ver_topo(&p);
+        }
+        else if (opcao == 6)
+        {
+            printf("Quantidade de elementos: %d\n", count_elementos_pilha(&p));
+        }
+        else if (opcao == 7)
+        {
+            printf("Soma dos elementos: %d\n", soma(&p));
+        }
+        else if (opcao == 8)
+        {
+            printf("Maior elemento da pilha: %d\n", maior(&p));
+        }
+        else if (opcao == 9)
+        {
+            printf("Menor elemento da pilha: %d\n", menor(&p));
+        }
+        else if (opcao == 0)
+        {
+            printf("Encerrando programa...\n");
+        }
+        else
+        {
+            printf("Opcao invalida!\n");
+        }
+
     } while (opcao != 0);
+
+    libera(&p);
     return 0;
-}
-
-void push(int valor){
-    Pilha *novo = malloc(sizeof(Pilha));
-    novo->dado = valor;
-    novo->prox = topo;
-    topo = novo;
-    printf("Valor adicionado: %d", valor);
-}
-
-void pop(){
-    if(topo == NULL){
-        printf("Pilha vazia");
-    }else{
-        Pilha *temporaria;
-        temporaria = topo;
-        topo = topo->prox;
-        printf("\nDesempilhando %d", temporaria->dado);
-        free(temporaria);
-    }
-}
-
-void listagem(){
-    if(topo == NULL){
-        printf("Pilha vazia!!");
-    }else{
-        Pilha *atual;
-        atual = topo;
-        while (atual != NULL){
-            printf("%d\n", atual->dado);
-            atual = atual->prox;
-        }
-    }
 }
