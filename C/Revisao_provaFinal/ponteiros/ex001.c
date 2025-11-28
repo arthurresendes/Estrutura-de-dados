9a79518d4d9092c0dee9b6cc3ce6042be6ecee41
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-27 21:40:53 -0300

    atualizae

--- C/Revisao_provaFinal/ponteiros/ex001.c
+++ C/Revisao_provaFinal/ponteiros/ex001.c
@@ -10,13 +10,17 @@ int main(){
     printf("Valor de x: %d\n", x);
     printf("Valor de x por ponteiro: %d\n", *ptr);
     printf("Valor de x por ponteiro do ponteiro: %d\n", **pptr);
+
     printf("Digite um novo valor para x: ");
     scanf("%d", ptr);
+
     printf("Valor de x: %d\n", x);
     printf("Valor de x por ponteiro: %d\n", *ptr);
     printf("Valor de x por ponteiro do ponteiro: %d\n", **pptr);
+
     printf("Digite um novo valor para x: ");
     scanf("%d", *pptr);
+
     printf("Valor de x: %d\n", x);
     printf("Valor de x por ponteiro: %d\n", *ptr);
     printf("Valor de x por ponteiro do ponteiro: %d\n", **pptr);
